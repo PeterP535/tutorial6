a50f34e52da62641804528826ec732a2092ac20b
Peter Putra Lesmana <158464449+PeterP535@users.noreply.github.com>
2025-03-21 13:03:40 +0700

    Initial commit

---
+++ target/debug/deps/tutorial6.d
@@ -0,0 +1,5 @@
+C:\Users\Peter\Desktop\ADPRO\tutorial6\target\debug\deps\tutorial6.exe: src\main.rs
+
+C:\Users\Peter\Desktop\ADPRO\tutorial6\target\debug\deps\tutorial6.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/tutorial6.d
@@ -0,0 +1,1 @@
+C:\Users\Peter\Desktop\ADPRO\tutorial6\target\debug\tutorial6.exe: C:\Users\Peter\Desktop\ADPRO\tutorial6\src\main.rs
